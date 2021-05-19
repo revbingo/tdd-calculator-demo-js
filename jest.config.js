3b983386d82219e288d8ff6b5275f2060ac71218
Mark Piper <mark@daizy.io>
2021-05-19 11:43:50 +0100

    Bootstrap

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
+  roots: [
+    "<rootDir>/src"
+  ],
+  testMatch: [
+    "**/__tests__/**/*.test.+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
   testEnvironment: 'node',
 };
--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -1,5 +1,6 @@
 describe('the calculator', () => {
   it('displays zero by default', () => {
-    
+    // Start here!
+    // const unit = new ... ?
   })
 })
